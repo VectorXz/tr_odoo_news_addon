1bab0f927134d1c211fd44388ec8e66d2605a692
vectorxz <vectorx2263@hotmail.com>
2020-07-13 22:21:23 +0700

    add search box

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,7 +7,7 @@ class News(http.Controller):
         '''/news''',
         '''/news/page/<int:page>'''
         ], auth='public', website=True)
-    def index(self, page=0, **kw):
+    def listing(self, page=0, **kw):
         Posts = http.request.env['trinityroots.news']
         posts_search = Posts.search([])
         posts_count = len(posts_search)
@@ -15,14 +15,26 @@ class News(http.Controller):
 
         posts_paged = Posts.search([], limit=8, offset=pager['offset'])
         return http.request.render('news.index', {
+            'all_categ': http.request.env['trinityroots.category'].search([]),
             'posts': posts_paged,
             'pager': pager
         })
 
     @http.route('/news/<model("trinityroots.news"):news>/', auth='public', website=True)
-    def teacher(self, news):
+    def read_news(self, news):
         return http.request.render('news.read_news', {
             'news_obj': news
+        })
+
+    @http.route('/news/search/', auth='public', website=True)
+    def search_news(self, keyword, category, **kw):
+        Posts = http.request.env['trinityroots.news']
+        if category == 'All':
+            posts_search = Posts.search([('title','ilike',keyword)])
+        else:
+            posts_search = Posts.search([('title','ilike',keyword), ('category.id','=',category)])
+        return http.request.render('news.search_result', {
+            'all_results': posts_search
         })
 
 #     @http.route('/news/news/objects/', auth='public')
